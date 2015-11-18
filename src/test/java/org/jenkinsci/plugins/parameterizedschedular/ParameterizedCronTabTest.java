7d89dd5f1853425543ebc1ecb2c0b8cc94b5e74d
Baptiste Mathus <batmat@batmat.net>
2015-11-18 16:27:10 +0100

    Force ENGLISH during tests
    Without that, the test would fail in a non-english environment.

--- src/test/java/org/jenkinsci/plugins/parameterizedschedular/ParameterizedCronTabTest.java
+++ src/test/java/org/jenkinsci/plugins/parameterizedschedular/ParameterizedCronTabTest.java
@@ -5,13 +5,27 @@ import static org.junit.Assert.assertTrue;
 import hudson.scheduler.CronTab;
 
 import java.util.GregorianCalendar;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Test;
+import org.jvnet.localizer.LocaleProvider;
 
 import com.google.common.collect.Maps;
 
 public class ParameterizedCronTabTest {
+
+	static
+	{
+		LocaleProvider.setProvider(new LocaleProvider()
+		{
+			@Override
+			public Locale get()
+			{
+				return Locale.ENGLISH;
+			}
+		});
+	}
 
 	@Test
 	public void ctor_happyPath() throws Exception {
@@ -23,6 +37,7 @@ public class ParameterizedCronTabTest {
 		assertEquals(parameters, testObject.getParameterValues());
 		assertTrue(testObject.check(new GregorianCalendar()));
 		assertTrue(testObject.checkSanity().startsWith("Do you really mean"));
+
 	}
 
 }
