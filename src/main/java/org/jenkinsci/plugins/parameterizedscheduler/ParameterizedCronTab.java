e485f08c5fd4c17cf3e96f1184ca47977f31f73d
Jonah Graham <jonah@kichwacoders.com>
2018-06-20 12:48:05 +0100

    [JENKINS-52068] Remove reflection and code duplication
    Instead use cronTabList which has accessible check() method

--- src/main/java/org/jenkinsci/plugins/parameterizedscheduler/ParameterizedCronTab.java
+++ src/main/java/org/jenkinsci/plugins/parameterizedscheduler/ParameterizedCronTab.java
@@ -1,18 +1,15 @@
 package org.jenkinsci.plugins.parameterizedscheduler;
 
-import static java.util.Calendar.DAY_OF_MONTH;
-import static java.util.Calendar.HOUR_OF_DAY;
-import static java.util.Calendar.MINUTE;
-import static java.util.Calendar.MONTH;
-import hudson.scheduler.CronTab;
-import hudson.scheduler.Hash;
-
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Map;
 
-import antlr.ANTLRException;
-
 import com.google.common.collect.Maps;
+
+import antlr.ANTLRException;
+import hudson.scheduler.CronTab;
+import hudson.scheduler.CronTabList;
+import hudson.scheduler.Hash;
 
 /**
  * this is a copy of {@link CronTab} with added parameters map support
@@ -22,22 +19,14 @@ import com.google.common.collect.Maps;
 public class ParameterizedCronTab {
 
 	private final Map<String, String> parameterValues;
-	private final long[] bits;
-	private final Integer dayOfWeek;
-	private final CronTab cronTab;
+	private final CronTabList cronTabList;
 
 	/**
-	 * look at this fragile happy-crappy! Is this better than duplicating all of the Crontab source-tree? you decide.
-	 * 
 	 * @param cronTab the crontab to use as a template
 	 * @param parameters the parameters in name=value key pairings
 	 */
 	public ParameterizedCronTab(CronTab cronTab, Map<String, String> parameters) {
-		this.cronTab = cronTab;
-		FieldAccessor fieldAccessor = new FieldAccessor();
-		bits = fieldAccessor.access(cronTab, "bits");
-		dayOfWeek = fieldAccessor.access(cronTab, "dayOfWeek");
-
+		cronTabList = new CronTabList(Collections.singleton(cronTab));
 		parameterValues = parameters;
 	}
 
@@ -60,29 +49,12 @@ public class ParameterizedCronTab {
 		return parameterValues;
 	}
 
-	/**
-	 * Returns true if n-th bit is on.
-	 */
-	private boolean checkBits(long bitMask, int n) {
-		return (bitMask | (1L << n)) == bitMask;
-	}
 
 	public boolean check(Calendar calendar) {
-		if (!checkBits(bits[0], calendar.get(MINUTE)))
-			return false;
-		if (!checkBits(bits[1], calendar.get(HOUR_OF_DAY)))
-			return false;
-		if (!checkBits(bits[2], calendar.get(DAY_OF_MONTH)))
-			return false;
-		if (!checkBits(bits[3], calendar.get(MONTH) + 1))
-			return false;
-		if (!checkBits(dayOfWeek, calendar.get(Calendar.DAY_OF_WEEK) - 1))
-			return false;
-
-		return true;
+		return cronTabList.check(calendar);
 	}
 
 	public String checkSanity() {
-		return cronTab.checkSanity();
+		return cronTabList.checkSanity();
 	}
 }
